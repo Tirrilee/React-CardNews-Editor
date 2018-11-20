4530fe39036b16ee11a167add777b3dad1dffeb4
jangwon <wkddnjset@gmail.com>
2018-11-21 02:36:54 +0900

    Component 개발 진행 중

--- src/containers/EditorPage/EditorPage.js
+++ src/containers/EditorPage/EditorPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap'
-
+import ImageCanvas from '../../components/ImageCanvas';
 // import './style.css'
 
 class EditorPage extends Component {
