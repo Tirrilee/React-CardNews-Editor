e9e0da2df288c942374da7b1fc17c879b7de5703
jangwon <wkddnjset@gmail.com>
2018-11-21 09:36:55 +0900

    ImageCanvas 개발 중

--- src/components/ImageCanvas/ImageCanvas.js
+++ src/components/ImageCanvas/ImageCanvas.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import { FormGroup, FormControl, HelpBlock } from 'react-bootstrap'
-import './style.css'
+import dog from '../../images/dog.jpg'
+// import './style.css'
 
 class ImageCanvas extends Component {
-  
+    componentDidMount() {
+      const canvas = this.refs.canvas
+      const ctx = canvas.getContext("2d")
+      const img = this.refs.image
+    }
     render() {
       // id, label, help를 제외한 값들은
       // 스프레드 연산자를 통해 ...props에 저장 
       var {img, ...props} = this.props
       return(
         <div>
-          
+          <canvas ref="canvas" width={640} height={425} />
+          <img ref="image" src={dog} className="hidden" />
         </div>
       )
       
--- src/containers/EditorPage/EditorPage.js
+++ src/containers/EditorPage/EditorPage.js
@@ -8,6 +8,7 @@ class EditorPage extends Component {
     	return (
       		<div className="EditorPage">
       			<p className="title">EditorPage</p>
+      			<ImageCanvas/>
       			<Button bsStyle="info">버튼</Button>
       		</div>
     	)
